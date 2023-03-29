d889ccc173e7134135a9726b9d81e63888cc5e53
PennyCiou <97937624+PennyCiou@users.noreply.github.com>
2023-03-29 14:47:01 +0800

    Update RTL9000Cx_sample_code.h
    updated.

--- RTL9000Cx_sample_code.h
+++ RTL9000Cx_sample_code.h
@@ -4,6 +4,7 @@
 #include "mdio2.h"
 #include "ht32f165x_it.h"
 
+
 #define DEBUG		1
 #if(DEBUG)
 	#define DBGMSG(x)		{printf x;}
@@ -39,6 +40,7 @@
 #define GENERAL_INTERRUPT_STATUS_LINK_STATUS_CHANGED			2
 #define GENERAL_INTERRUPT_STATUS_PTP_EVENT						3
 #define GENERAL_INTERRUPT_STATUS_PHY_FATAL_ERROR				4
+#define GENERAL_INTERRUPT_STATUS_MACSEC_EVENT				5
 
 #define GENERAL_INTERRUPT_SUB_STATUS_SLEEP_ACK				0
 #define GENERAL_INTERRUPT_SUB_STATUS_SLEEP_FAIL			1
@@ -92,11 +94,6 @@ typedef enum {
 	OP_CMD_GOTO_SLEEP = 3
 } OP_CMD;
 
-typedef enum {
-	Speed_LinkDown = 0,
-	Speed_100BaseT1 = 1,
-	Speed_1000BaseT1,		// 2
-} Link_Speed;
 
 typedef enum {
 	PCS_STATE_DISABLE_TRANSMITTER	= 0x01,
@@ -142,72 +139,69 @@ typedef enum {
 
 void wait_x_ms( u32 count ); 
 
-u8 RTL9010Bx_Initial_Configuration(void);
-u8 RTL9010Bx_Initial_with_EEE_Configuration(void);
-u8 RTL9010Bx_Initial_Configuration_Check(void);
-u8 RTL9010Bx_Initial_with_EEE_Configuration_Check(void);
-u8 RTL9010Bx_GetLinkStatus(u8 SpeedType);
-u8 RTL9010Bx_Soft_Reset(void);
-u8 RTL9010Bx_CableFaultLocationAndDiagnosis(u16* cable_length);
-u8 RTL9010Bx_SQI(u8 SpeedType);
-u8 RTL9010Bx_Check_Linkup(void);
-u8 RTL9010Bx_Sleep_request(void);
-u8 RTL9010Bx_Sleep_initial(void);
-u8 RTL9010Bx_Sleep_reject(void);
-u8 RTL9010Bx_General_Interrupt_status(u16* general_int_status);
-u8 RTL9010Bx_General_purpose_sub_status(u16* general_sub_status);
-u8 RTL9010Bx_General_Interrupt_setting(void);
-u8 RTL9010Bx_OP_Interrupt_setting(void);
-u8 RTL9010Bx_OP_Interrupt_status(u32* op_status);
-u8 RTL9010Bx_HOSTCMD_send_rwake(void);
-u8 RTL9010Bx_Set_OP_state(u8 op_cmd);
-u8 RTL9010Bx_Get_OP_state(void);
-u8 RTL9010Bx_Disable_lwake_to_rwake(void);
-u8 RTL9010Bx_Enable_lwake_to_rwake(void);
-u8 RTL9010Bx_Check_lwake_to_rwake(void);
-u8 RTL9010Bx_Check_rwake_en_dis(void);
-u8 RTL9010Bx_Disable_rwake(void);
-u8 RTL9010Bx_Enable_rwake(void);
-u8 RTL9010Bx_Get_lwake_edge(void);
-u8 RTL9010Bx_Set_lwake_edge(u8 edge_select);
-u8 RTL9010Bx_Sleep_cap_check(void);
-u8 RTL9010Bx_xMII_driving_strength(u8 RGMII_Voltage);
-u8 RTL9010Bx_PHY_ready(void);
-u8 RTL9010Bx_Get_PCS_status(u8 SpeedType);
-u8 RTL9010Bx_PCS_loopback(void);
-u8 RTL9010Bx_MDI_loopback(void);
-u8 RTL9010Bx_Remote_loopback(void);
-u8 RTL9010Bx_IOL_test(u8 TestMode_selection, u8 SpeedType);
+u8 RTL9000Cx_Initial_Configuration(void);
+u8 RTL9000Cx_Initial_Configuration_Check(void);
+u8 RTL9000Cx_GetLinkStatus(void);
+u8 RTL9000Cx_Soft_Reset(void);
+u8 RTL9000Cx_CableFaultLocationAndDiagnosis(u16* cable_length);
+u8 RTL9000Cx_SQI(void);
+u8 RTL9000Cx_Check_Linkup(void);
+u8 RTL9000Cx_Sleep_request(void);
+u8 RTL9000Cx_Sleep_initial(void);
+u8 RTL9000Cx_Sleep_reject(void);
+u8 RTL9000Cx_General_Interrupt_status(u16* general_int_status);
+u8 RTL9000Cx_General_purpose_sub_status(u16* general_sub_status);
+u8 RTL9000Cx_General_Interrupt_setting(void);
+u8 RTL9000Cx_OP_Interrupt_setting(void);
+u8 RTL9000Cx_OP_Interrupt_status(u32* op_status);
+u8 RTL9000Cx_HOSTCMD_send_rwake(void);
+u8 RTL9000Cx_Set_OP_state(u8 op_cmd);
+u8 RTL9000Cx_Get_OP_state(void);
+u8 RTL9000Cx_Disable_lwake_to_rwake(void);
+u8 RTL9000Cx_Enable_lwake_to_rwake(void);
+u8 RTL9000Cx_Check_lwake_to_rwake(void);
+u8 RTL9000Cx_Check_rwake_en_dis(void);
+u8 RTL9000Cx_Disable_rwake(void);
+u8 RTL9000Cx_Enable_rwake(void);
+u8 RTL9000Cx_Get_lwake_edge(void);
+u8 RTL9000Cx_Set_lwake_edge(u8 edge_select);
+u8 RTL9000Cx_Sleep_cap_check(void);
+u8 RTL9000Cx_xMII_driving_strength(u8 RGMII_Voltage);
+u8 RTL9000Cx_PHY_ready(void);
+u8 RTL9000Cx_Get_PCS_status(void);
+u8 RTL9000Cx_PCS_loopback(void);
+u8 RTL9000Cx_MDI_loopback(void);
+u8 RTL9000Cx_Remote_loopback(void);
+u8 RTL9000Cx_IOL_test(u8 TestMode_selection);
 
 
-u8 RTL9010Bx_MACsec_Enable(u8 Control);
-u8 RTL9010Bx_MACsec_Egress_Check_Access_Correct(void);
-u8 RTL9010Bx_MACsec_Ingress_Check_Access_Correct(void);
-u32 RTL9010Bx_MACsec_Egress_Read(u16 Address);
-u32 RTL9010Bx_MACsec_Ingress_Read(u16 Address);
-u8 RTL9010Bx_MACsec_Egress_Write(u16 Address, u32 reg_value);
-u8 RTL9010Bx_MACsec_Ingress_Write(u16 Address, u32 reg_value);
+u8 RTL9000Cx_MACsec_Enable(u8 Control);
+u8 RTL9000Cx_MACsec_Egress_Check_Access_Correct(void);
+u8 RTL9000Cx_MACsec_Ingress_Check_Access_Correct(void);
+u32 RTL9000Cx_MACsec_Egress_Read(u16 Address);
+u32 RTL9000Cx_MACsec_Ingress_Read(u16 Address);
+u8 RTL9000Cx_MACsec_Egress_Write(u16 Address, u32 reg_value);
+u8 RTL9000Cx_MACsec_Ingress_Write(u16 Address, u32 reg_value);
 
-u8 RTL9010Bx_Start_MACsec_Function(void);
+u8 RTL9000Cx_Start_MACsec_Function(void);
 
-u32 RTL9010Bx_Monitor_SAK_Packet_Number(u8 SAK_n);
-u8 RTL9010Bx_Switch_Egress_SAK(u8 SW_SAK);
-u8 RTL9010Bx_Update_SAK1(u8 Dir);
-u8 RTL9010Bx_Update_SAK2(u8 Dir);
+u32 RTL9000Cx_Monitor_SAK_Packet_Number(u8 SAK_n);
+u8 RTL9000Cx_Switch_Egress_SAK(u8 SW_SAK);
+u8 RTL9000Cx_Update_SAK1(u8 Dir);
+u8 RTL9000Cx_Update_SAK2(u8 Dir);
 
-u32 RTL9010Bx_Egress_MIBCounter_Encrypted_OutPktBytes(u8 SAK_n);
-u32 RTL9010Bx_Egress_MIBCounter_Encrypted_OutPkts(u8 SAK_n);
-u32 RTL9010Bx_Egress_MIBCounter_Encrypted_OutPktsTooLong(u8 SAK_n);
-u32 RTL9010Bx_Ingress_MIBCounter_Decrypted_InPktBytes(u8 SAK_n);
-u32 RTL9010Bx_Ingress_MIBCounter_Decrypted_InPktsOK(u8 SAK_n);
-u32 RTL9010Bx_Ingress_MIBCounter_Decrypted_InPktsNotValid(u8 SAK_n);
-u32 RTL9010Bx_Ingress_MIBCounter_Decrypted_InPktsDelayed(u8 SAK_n);
-u32 RTL9010Bx_Ingress_MIBCounter_Decrypted_InPktsLate(u8 SAK_n);
-u32 RTL9010Bx_Ingress_MIBCounter_Decrypted_InPktsInvalid(u8 SAK_n);
-u32 RTL9010Bx_Ingress_MIBCounter_Decrypted_InPktsNotUsingSA(u8 SAK_n);
-u32 RTL9010Bx_Ingress_MIBCounter_Decrypted_InPktsUnusedSA(u8 SAK_n);
-u32 RTL9010Bx_Ingress_MIBCounter_Decrypted_InPktsUntaggedHit(u8 SAK_n);
+u32 RTL9000Cx_Egress_MIBCounter_Encrypted_OutPktBytes(u8 SAK_n);
+u32 RTL9000Cx_Egress_MIBCounter_Encrypted_OutPkts(u8 SAK_n);
+u32 RTL9000Cx_Egress_MIBCounter_Encrypted_OutPktsTooLong(u8 SAK_n);
+u32 RTL9000Cx_Ingress_MIBCounter_Decrypted_InPktBytes(u8 SAK_n);
+u32 RTL9000Cx_Ingress_MIBCounter_Decrypted_InPktsOK(u8 SAK_n);
+u32 RTL9000Cx_Ingress_MIBCounter_Decrypted_InPktsNotValid(u8 SAK_n);
+u32 RTL9000Cx_Ingress_MIBCounter_Decrypted_InPktsDelayed(u8 SAK_n);
+u32 RTL9000Cx_Ingress_MIBCounter_Decrypted_InPktsLate(u8 SAK_n);
+u32 RTL9000Cx_Ingress_MIBCounter_Decrypted_InPktsInvalid(u8 SAK_n);
+u32 RTL9000Cx_Ingress_MIBCounter_Decrypted_InPktsNotUsingSA(u8 SAK_n);
+u32 RTL9000Cx_Ingress_MIBCounter_Decrypted_InPktsUnusedSA(u8 SAK_n);
+u32 RTL9000Cx_Ingress_MIBCounter_Decrypted_InPktsUntaggedHit(u8 SAK_n);
 
-u8 RTL9010Bx_MACsec_Initial_Configuration_example(void);
-
-
+u8 RTL9000Cx_MACsec_Initial_Configuration_example_1(void);
+u8 RTL9000Cx_MACsec_Initial_Configuration_example_2(void);
